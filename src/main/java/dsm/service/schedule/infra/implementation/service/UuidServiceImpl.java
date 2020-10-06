6b6ebfa9521f67e66fd6debbcf2094b21aefc2aa
Maly <migsking@naver.com>
2020-10-06 23:52:23 +0900

    [ADD] UuidServiceImpl

--- src/main/java/dsm/service/schedule/infra/implementation/service/UuidServiceImpl.java
+++ src/main/java/dsm/service/schedule/infra/implementation/service/UuidServiceImpl.java
@@ -1,20 +1,42 @@
 package dsm.service.schedule.infra.implementation.service;
 
+import dsm.service.schedule.domain.entity.Schedule;
+import dsm.service.schedule.domain.repository.ScheduleRepository;
 import dsm.service.schedule.domain.service.UuidService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+import java.util.Random;
+
+@Component
+@AllArgsConstructor
 public class UuidServiceImpl implements UuidService {
+    private final ScheduleRepository scheduleRepository;
+
     @Override
     public boolean checkUuid(String uuid) {
-        return false;
+        return scheduleRepository.findById(uuid).isPresent();
     }
 
     @Override
     public String generateRandomKey() {
-        return null;
+        String SALTCHARS = "1234567890";
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+        while (salt.length() < 12) { // length of the random string.
+            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+        }
+        return salt.toString();
     }
 
     @Override
     public String generateUuid() {
-        return null;
+        String uuid = "schedule-"+generateRandomKey();
+        while (checkUuid(uuid)) {
+            uuid = "schedule-" + generateRandomKey();
+        }
+        return uuid;
     }
 }

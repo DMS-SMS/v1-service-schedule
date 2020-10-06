838c2e3579e1c4f09edf09aa72fadc9c811c3c71
Maly <migsking@naver.com>
2020-10-06 22:18:18 +0900

    [CREATE] Create the Get Schedule Usecase

---
+++ src/main/java/dsm/service/schedule/domain/usecase/GetScheduleUseCase.java
@@ -0,0 +1,9 @@
+package dsm.service.schedule.domain.usecase;
+
+import dsm.service.schedule.domain.entity.Schedule;
+
+import java.util.Optional;
+
+public interface GetScheduleUseCase {
+    public Iterable<Schedule> run();
+}

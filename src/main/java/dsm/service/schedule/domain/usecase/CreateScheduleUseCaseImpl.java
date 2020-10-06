505662a55011932336630f9cc5916d51c40f4986
Maly <migsking@naver.com>
2020-10-06 23:01:11 +0900

    [CREATE] Create Schedule UseCase

---
+++ src/main/java/dsm/service/schedule/domain/usecase/CreateScheduleUseCaseImpl.java
@@ -0,0 +1,31 @@
+package dsm.service.schedule.domain.usecase;
+
+
+import dsm.service.schedule.domain.entity.Schedule;
+import dsm.service.schedule.domain.repository.ScheduleRepository;
+import dsm.service.schedule.domain.service.UuidService;
+import jdk.vm.ci.meta.Local;
+import lombok.AllArgsConstructor;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+public class CreateScheduleUseCaseImpl implements CreateScheduleUseCase {
+    final private ScheduleRepository scheduleRepository;
+
+    final private UuidService uuidService;
+
+    @Override
+    public void run(
+            String teacherUuid, String detail, LocalDate startTime, LocalDate endTime) {
+        scheduleRepository.save(
+                Schedule.builder()
+                        .uuid(uuidService.generateUuid())
+                        .teacherUuid(teacherUuid)
+                        .detail(detail)
+                        .startDate(startTime)
+                        .endDate(endTime)
+                        .build()
+        );
+    }
+}

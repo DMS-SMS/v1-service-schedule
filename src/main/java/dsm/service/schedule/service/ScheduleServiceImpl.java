0d0f39da4410f99595b731ae066ed698fd7d4b48
Maly <migsking@naver.com>
2020-10-06 23:08:32 +0900

    [CREATE] Create Schedule Service

--- src/main/java/dsm/service/schedule/service/ScheduleServiceImpl.java
+++ src/main/java/dsm/service/schedule/service/ScheduleServiceImpl.java
@@ -1,7 +1,10 @@
 package dsm.service.schedule.service;
 
 import dsm.service.schedule.domain.entity.Schedule;
+import dsm.service.schedule.domain.usecase.CreateScheduleUseCase;
 import dsm.service.schedule.domain.usecase.GetScheduleUseCase;
+import dsm.service.schedule.proto.CreateScheduleRequest;
+import dsm.service.schedule.proto.DefaultScheduleResponse;
 import dsm.service.schedule.proto.GetScheduleResponse;
 import dsm.service.schedule.service.mapper.ScheduleMapper;
 import lombok.AllArgsConstructor;
@@ -12,6 +15,7 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class ScheduleServiceImpl implements ScheduleService {
     private final GetScheduleUseCase getScheduleUseCase;
+    private final CreateScheduleUseCase createScheduleUseCase;
 
     private final ScheduleMapper scheduleMapper;
 
@@ -20,5 +24,11 @@ public class ScheduleServiceImpl implements ScheduleService {
         Iterable<Schedule> schedules = getScheduleUseCase.run();
         GetScheduleResponse.Builder response = scheduleMapper.getScheduleMapper(schedules);
         return response.build();
+    }
+
+    @Override
+    public DefaultScheduleResponse createScheduleService(CreateScheduleRequest request) {
+        createScheduleUseCase.run(request.getUuid(), request.getDetail(), request.getStartDate(), request.getEndDate());
+        return DefaultScheduleResponse.newBuilder().setStatus(201)
     }
 }

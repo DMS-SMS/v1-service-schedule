e6334c4186dfe9dbb48be28501f238e7c736ea94
Maly <migsking@naver.com>
2020-10-07 22:07:56 +0900

    [ADD] #16 add the validate uuid logic

--- src/main/java/dsm/service/schedule/domain/usecase/UpdateScheduleUseCaseImpl.java
+++ src/main/java/dsm/service/schedule/domain/usecase/UpdateScheduleUseCaseImpl.java
@@ -2,6 +2,7 @@ package dsm.service.schedule.domain.usecase;
 
 import dsm.service.schedule.domain.entity.Schedule;
 import dsm.service.schedule.domain.repository.ScheduleRepository;
+import dsm.service.schedule.domain.repository.TeacherRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -13,11 +14,12 @@ import java.util.Optional;
 @AllArgsConstructor
 public class UpdateScheduleUseCaseImpl implements UpdateScheduleUseCase {
     private final ScheduleRepository scheduleRepository;
+    private final TeacherRepository teacherRepository;
 
     @Override
     public void run(String teacherUuid, String scheduleUuid, String detail, Long startTime, Long endTime) {
-        scheduleRepository.findById(scheduleUuid).ifPresent(
-                schedule -> {
+        teacherRepository.findById(teacherUuid)
+                .flatMap(teacher -> scheduleRepository.findById(scheduleUuid)).ifPresent(schedule -> {
                     schedule.setDetail(detail);
                     schedule.setStartDate(LocalDate.ofEpochDay(startTime));
                     schedule.setEndDate(LocalDate.ofEpochDay(endTime));

1884938ce90fc5ce029afb68386d313993531606
Maly <migsking@naver.com>
2020-10-07 19:03:59 +0900

    [CREATE] Auth Mapper

---
+++ src/main/java/dsm/service/schedule/infra/auth/mapper/AuthMapper.java
@@ -0,0 +1,34 @@
+package dsm.service.schedule.infra.auth.mapper;
+
+import dsm.service.schedule.domain.entity.Account;
+import dsm.service.schedule.proto.GetTeacherInformWithUUIDRequest;
+import dsm.service.schedule.proto.GetTeacherInformWithUUIDResponse;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+
+@Component
+public class AuthMapper {
+    public GetTeacherInformWithUUIDRequest authGetInformRequestMapper(String uuid) {
+        System.out.println(uuid);
+        return GetTeacherInformWithUUIDRequest.newBuilder()
+                .setTeacherUUID(uuid)
+                .setUUID(uuid)
+                .build();
+    }
+
+    public Optional<Account> authGetInformResponseMapper(GetTeacherInformWithUUIDResponse response) {
+        System.out.println(response);
+        if (response.getStatus() != 200) {
+            return Optional.empty();
+        }
+
+        return Optional.of(Account.builder()
+                .grade(response.getGrade())
+                .group(response.getGroup())
+                .name(response.getName())
+                .phoneNumber(response.getPhoneNumber())
+                .build());
+    }
+}

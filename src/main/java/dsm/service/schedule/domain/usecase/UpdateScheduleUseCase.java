c032ceb1011536a3183e9dc6e2b06da36dc535a1
Maly <migsking@naver.com>
2020-10-07 13:33:02 +0900

    [CREATE] UpdateScheduleUseCase

--- src/main/java/dsm/service/schedule/domain/usecase/UpdateScheduleUseCase.java
+++ src/main/java/dsm/service/schedule/domain/usecase/UpdateScheduleUseCase.java
@@ -1,4 +1,7 @@
 package dsm.service.schedule.domain.usecase;
 
 public interface UpdateScheduleUseCase {
+    public void run(
+            String teacherUuid, String scheduleUuid, String detail, Long startTime, Long endTime
+    );
 }

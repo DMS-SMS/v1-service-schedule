8eb28ef3ed8d62d3ab89a5753af8801d7244acce
Maly <migsking@naver.com>
2020-10-07 13:59:29 +0900

    [ADD] DeleteService 구현

--- src/main/java/dsm/service/schedule/service/ScheduleServiceImpl.java
+++ src/main/java/dsm/service/schedule/service/ScheduleServiceImpl.java
@@ -2,12 +2,10 @@ package dsm.service.schedule.service;
 
 import dsm.service.schedule.domain.entity.Schedule;
 import dsm.service.schedule.domain.usecase.CreateScheduleUseCase;
+import dsm.service.schedule.domain.usecase.DeleteScheduleUseCase;
 import dsm.service.schedule.domain.usecase.GetScheduleUseCase;
 import dsm.service.schedule.domain.usecase.UpdateScheduleUseCase;
-import dsm.service.schedule.proto.CreateScheduleRequest;
-import dsm.service.schedule.proto.DefaultScheduleResponse;
-import dsm.service.schedule.proto.GetScheduleResponse;
-import dsm.service.schedule.proto.UpdateScheduleRequest;
+import dsm.service.schedule.proto.*;
 import dsm.service.schedule.service.mapper.ScheduleMapper;
 import lombok.AllArgsConstructor;
 import org.hibernate.sql.Update;
@@ -20,6 +18,7 @@ public class ScheduleServiceImpl implements ScheduleService {
     private final GetScheduleUseCase getScheduleUseCase;
     private final CreateScheduleUseCase createScheduleUseCase;
     private final UpdateScheduleUseCase updateScheduleUseCase;
+    private final DeleteScheduleUseCase deleteScheduleUseCase;
 
     private final ScheduleMapper scheduleMapper;
 
@@ -45,6 +44,12 @@ public class ScheduleServiceImpl implements ScheduleService {
                 request.getStartDate(),
                 request.getEndDate()
         );
+        return DefaultScheduleResponse.newBuilder().setStatus(200).build();
+    }
+
+    @Override
+    public DefaultScheduleResponse deleteScheduleService(DeleteScheduleRequest request) {
+        deleteScheduleUseCase.run(request.getUuid(), request.getScheduleUUID());
         return DefaultScheduleResponse.newBuilder().setStatus(200).build();
     }
 }

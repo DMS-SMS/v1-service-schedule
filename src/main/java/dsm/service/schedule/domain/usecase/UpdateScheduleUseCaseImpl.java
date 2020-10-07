7651a9055d3dca2497d0f2845ebcdb7e6d603ec2
Maly <migsking@naver.com>
2020-10-07 13:33:21 +0900

    [ADD] UpdateScheduleUseCaseImpl implementation

--- src/main/java/dsm/service/schedule/domain/usecase/UpdateScheduleUseCaseImpl.java
+++ src/main/java/dsm/service/schedule/domain/usecase/UpdateScheduleUseCaseImpl.java
@@ -1,4 +1,30 @@
 package dsm.service.schedule.domain.usecase;
 
-public class UpdateScheduleUseCaseImpl {
+import dsm.service.schedule.domain.entity.Schedule;
+import dsm.service.schedule.domain.repository.ScheduleRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+
+@Component
+@AllArgsConstructor
+public class UpdateScheduleUseCaseImpl implements UpdateScheduleUseCase {
+    private final ScheduleRepository scheduleRepository;
+
+    @Override
+    public void run(String teacherUuid, String scheduleUuid, String detail, Long startTime, Long endTime) {
+        scheduleRepository.findById(scheduleUuid).ifPresent(
+                schedule -> {
+                    schedule.setDetail(detail);
+                    schedule.setStartDate(LocalDate.ofEpochDay(startTime));
+                    schedule.setEndDate(LocalDate.ofEpochDay(endTime));
+                    scheduleRepository.save(
+                            schedule
+                    );
+                }
+        );
+    }
 }

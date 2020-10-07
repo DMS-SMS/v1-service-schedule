75134f60eac17a0849d3c9ff4223aeec78a01dcf
Maly <migsking@naver.com>
2020-10-07 13:56:44 +0900

    [ADD] add implementation of update service

--- src/main/java/dsm/service/schedule/service/ScheduleServiceImpl.java
+++ src/main/java/dsm/service/schedule/service/ScheduleServiceImpl.java
@@ -3,11 +3,14 @@ package dsm.service.schedule.service;
 import dsm.service.schedule.domain.entity.Schedule;
 import dsm.service.schedule.domain.usecase.CreateScheduleUseCase;
 import dsm.service.schedule.domain.usecase.GetScheduleUseCase;
+import dsm.service.schedule.domain.usecase.UpdateScheduleUseCase;
 import dsm.service.schedule.proto.CreateScheduleRequest;
 import dsm.service.schedule.proto.DefaultScheduleResponse;
 import dsm.service.schedule.proto.GetScheduleResponse;
+import dsm.service.schedule.proto.UpdateScheduleRequest;
 import dsm.service.schedule.service.mapper.ScheduleMapper;
 import lombok.AllArgsConstructor;
+import org.hibernate.sql.Update;
 import org.springframework.stereotype.Service;
 
 
@@ -16,6 +19,7 @@ import org.springframework.stereotype.Service;
 public class ScheduleServiceImpl implements ScheduleService {
     private final GetScheduleUseCase getScheduleUseCase;
     private final CreateScheduleUseCase createScheduleUseCase;
+    private final UpdateScheduleUseCase updateScheduleUseCase;
 
     private final ScheduleMapper scheduleMapper;
 
@@ -30,5 +34,17 @@ public class ScheduleServiceImpl implements ScheduleService {
     public DefaultScheduleResponse createScheduleService(CreateScheduleRequest request) {
         createScheduleUseCase.run(request.getUuid(), request.getDetail(), request.getStartDate(), request.getEndDate());
         return DefaultScheduleResponse.newBuilder().setStatus(201).build();
+    }
+
+    @Override
+    public DefaultScheduleResponse updateScheduleService(UpdateScheduleRequest request) {
+        updateScheduleUseCase.run(
+                request.getUuid(),
+                request.getScheduleUUID(),
+                request.getDetail(),
+                request.getStartDate(),
+                request.getEndDate()
+        );
+        return DefaultScheduleResponse.newBuilder().setStatus(200).build();
     }
 }

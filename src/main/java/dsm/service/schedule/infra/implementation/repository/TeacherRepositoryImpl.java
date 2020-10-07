744d99a44a2ac65c9db99b22d729ab23b023cca5
Maly <migsking@naver.com>
2020-10-07 18:57:08 +0900

    [CREATE] Teacher Repository

---
+++ src/main/java/dsm/service/schedule/infra/implementation/repository/TeacherRepositoryImpl.java
@@ -0,0 +1,26 @@
+package dsm.service.schedule.infra.implementation.repository;
+
+import dsm.service.schedule.domain.entity.Account;
+import dsm.service.schedule.domain.repository.TeacherRepository;
+import dsm.service.schedule.infra.auth.AuthHandler;
+import dsm.service.schedule.infra.auth.mapper.AuthMapper;
+import dsm.service.schedule.proto.GetTeacherInformWithUUIDResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+@AllArgsConstructor
+public class TeacherRepositoryImpl implements TeacherRepository {
+    private final AuthHandler authHandler;
+
+    private final AuthMapper authMapper;
+
+    @Override
+    public Optional<Account> findById(String uuid) {
+        GetTeacherInformWithUUIDResponse response = authHandler.getTeacherInform(authMapper.authGetInformRequestMapper(uuid));
+        return authMapper.authGetInformResponseMapper(response);
+    }
+}

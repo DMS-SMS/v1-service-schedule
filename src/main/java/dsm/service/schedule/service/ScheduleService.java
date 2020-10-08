e6e847ebee4b3f8d343d117265724c94744ffbc1
Maly <migsking@naver.com>
2020-10-08 20:12:24 +0900

    [ADD] #20 getTimeTableService 추가

--- src/main/java/dsm/service/schedule/service/ScheduleService.java
+++ src/main/java/dsm/service/schedule/service/ScheduleService.java
@@ -1,6 +1,7 @@
 package dsm.service.schedule.service;
 
 import dsm.service.schedule.domain.usecase.GetScheduleUseCase;
+import dsm.service.schedule.domain.usecase.GetTimeTableUseCase;
 import dsm.service.schedule.proto.*;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,6 +9,8 @@ import org.springframework.stereotype.Service;
 @Service
 public interface ScheduleService {
     public GetScheduleResponse getScheduleService();
+
+    public GetTimeTableResponse getTimeTableService(GetTimeTableRequest request);
 
     public DefaultScheduleResponse createScheduleService(CreateScheduleRequest request);
 

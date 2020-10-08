11ed5cce87cd1ec527845191ce8dcdc00fafdc59
Maly <migsking@naver.com>
2020-10-08 20:17:59 +0900

    [CREATE] #16 StudentRepository 추가

---
+++ src/main/java/dsm/service/schedule/domain/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package dsm.service.schedule.domain.repository;
+
+import dsm.service.schedule.domain.entity.Account;
+
+import java.util.Optional;
+
+public interface StudentRepository {
+    Optional<Account> findById(String uuid);
+}

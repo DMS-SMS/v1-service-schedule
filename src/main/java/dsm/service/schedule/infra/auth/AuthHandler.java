004e387c61fb20d46d57ff0ab911ea20a3475348
Maly <migsking@naver.com>
2020-10-08 20:16:03 +0900

    [MODIFY] #16 Student , Teacher Auth service 분리

--- src/main/java/dsm/service/schedule/infra/auth/AuthHandler.java
+++ src/main/java/dsm/service/schedule/infra/auth/AuthHandler.java
@@ -1,8 +1,6 @@
 package dsm.service.schedule.infra.auth;
 
-import dsm.service.schedule.proto.AuthTeacherGrpc;
-import dsm.service.schedule.proto.GetTeacherInformWithUUIDRequest;
-import dsm.service.schedule.proto.GetTeacherInformWithUUIDResponse;
+import dsm.service.schedule.proto.*;
 import io.grpc.ClientInterceptors;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
@@ -14,22 +12,33 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AuthHandler {
+    private String host = "127.0.0.1";
+    private Integer port = 10071;
+    private String xRequestId = "f9ed4675f1c53513c61a3b3b4e25b4c0";
+    private String spanContext = "1:1:0:0";
+    private ManagedChannel channel = ManagedChannelBuilder.forAddress(host, port)
+            .usePlaintext().build();
+    private Metadata metadata = new Metadata();
+
     public GetTeacherInformWithUUIDResponse getTeacherInform(
-            GetTeacherInformWithUUIDRequest request) {
-        String host = "127.0.0.1";
-        Integer port = 10071;
-        String xRequestId = "f9ed4675f1c53513c61a3b3b4e25b4c0";
-        String spanContext = "1:1:0:0";
-
-        ManagedChannel channel = ManagedChannelBuilder.forAddress(host, port)
-                .usePlaintext().build();
-
+            GetTeacherInformWithUUIDRequest request
+    ) {
         AuthTeacherGrpc.AuthTeacherBlockingStub authTeacherStub = AuthTeacherGrpc.newBlockingStub(channel);
 
-        Metadata metadata = new Metadata();
         metadata.put(Metadata.Key.of("x-request-id", Metadata.ASCII_STRING_MARSHALLER), xRequestId);
         metadata.put(Metadata.Key.of("span-context", Metadata.ASCII_STRING_MARSHALLER), spanContext);
 
         return MetadataUtils.attachHeaders(authTeacherStub, metadata).getTeacherInformWithUUID(request);
+    }
+
+    public GetStudentInformWithUUIDResponse getStudentInform(
+            GetStudentInformWithUUIDRequest request
+    ) {
+        AuthStudentGrpc.AuthStudentBlockingStub authStudentStub = AuthStudentGrpc.newBlockingStub(channel);
+
+        metadata.put(Metadata.Key.of("x-request-id", Metadata.ASCII_STRING_MARSHALLER), xRequestId);
+        metadata.put(Metadata.Key.of("span-context", Metadata.ASCII_STRING_MARSHALLER), spanContext);
+
+        return MetadataUtils.attachHeaders(authStudentStub, metadata).getStudentInformWithUUID(request);
     }
 }

bbaae6fe134b4ad434b4e4f1c756547965002cc7
Maly <migsking@naver.com>
2020-10-08 20:14:07 +0900

    [ADD] #23 Optional로 조금더 깔끔하게 정리

--- src/main/java/dsm/service/schedule/infra/auth/mapper/AuthMapper.java
+++ src/main/java/dsm/service/schedule/infra/auth/mapper/AuthMapper.java
@@ -1,6 +1,8 @@
 package dsm.service.schedule.infra.auth.mapper;
 
 import dsm.service.schedule.domain.entity.Account;
+import dsm.service.schedule.proto.GetStudentInformWithUUIDRequest;
+import dsm.service.schedule.proto.GetStudentInformWithUUIDResponse;
 import dsm.service.schedule.proto.GetTeacherInformWithUUIDRequest;
 import dsm.service.schedule.proto.GetTeacherInformWithUUIDResponse;
 import org.springframework.stereotype.Component;
@@ -10,14 +12,27 @@ import java.util.Optional;
 
 @Component
 public class AuthMapper {
-    public GetTeacherInformWithUUIDRequest authGetInformRequestMapper(String uuid) {
+    public GetTeacherInformWithUUIDRequest authGetTeacherInformRequestMapper(String uuid) {
         return GetTeacherInformWithUUIDRequest.newBuilder()
                 .setTeacherUUID(uuid)
                 .setUUID(uuid)
                 .build();
     }
 
-    public Optional<Account> authGetInformResponseMapper(GetTeacherInformWithUUIDResponse response) {
+    public Optional<Account> authGetTeacherInformResponseMapper(GetTeacherInformWithUUIDResponse response) {
+        if (response.getStatus() != 200) {
+            return Optional.empty();
+        }
+
+        return Optional.of(Account.builder()
+                .grade(response.getGrade())
+                .group(response.getGroup())
+                .name(response.getName())
+                .phoneNumber(response.getPhoneNumber())
+                .build());
+    }
+
+
         if (response.getStatus() != 200) {
             return Optional.empty();
         }

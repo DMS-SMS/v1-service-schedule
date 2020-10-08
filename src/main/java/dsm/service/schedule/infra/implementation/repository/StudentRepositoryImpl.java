fc6a51b6b3a409dcbd6fd46e49c876f6b6d788cd
Maly <migsking@naver.com>
2020-10-08 20:15:04 +0900

    [CREATE] #16 StudentRepositoryImpl 생성

---
+++ src/main/java/dsm/service/schedule/infra/implementation/repository/StudentRepositoryImpl.java
@@ -0,0 +1,30 @@
+package dsm.service.schedule.infra.implementation.repository;
+
+import dsm.service.schedule.domain.entity.Account;
+import dsm.service.schedule.domain.repository.StudentRepository;
+import dsm.service.schedule.infra.auth.AuthHandler;
+import dsm.service.schedule.infra.auth.mapper.AuthMapper;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+@AllArgsConstructor
+public class StudentRepositoryImpl implements StudentRepository {
+    private final AuthMapper authMapper;
+
+    private final AuthHandler authHandler;
+
+    @Override
+    public Optional<Account> findById(String uuid) {
+        try {
+            return authMapper.authGetStudentInformResponseMapper(
+                    authHandler.getStudentInform(authMapper.authGetStudentInformRequestMapper(uuid))
+            );
+        } catch (Exception ignored) {
+            return Optional.empty();
+        }
+    }
+}

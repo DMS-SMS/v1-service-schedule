360e72ea814873e93de5bc6577eb277451b0aa99
Maly <migsking@naver.com>
2020-10-10 15:29:32 +0900

    [ADD] #20 Timetable 관련 AOP 추가

--- src/main/java/dsm/service/schedule/service/aop/ErrorHandling.java
+++ src/main/java/dsm/service/schedule/service/aop/ErrorHandling.java
@@ -3,6 +3,7 @@ package dsm.service.schedule.service.aop;
 import dsm.service.schedule.domain.exception.BusinessException;
 import dsm.service.schedule.proto.DefaultScheduleResponse;
 import dsm.service.schedule.proto.GetScheduleResponse;
+import dsm.service.schedule.proto.GetTimeTableResponse;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -15,6 +16,9 @@ public class ErrorHandling {
     @Pointcut("execution(* dsm.service.schedule.service.ScheduleServiceImpl.getScheduleService(..))")
     private void getSchedulePointCut() {}
 
+    @Pointcut("execution(* dsm.service.schedule.service.ScheduleServiceImpl.getTimeTableService(..))")
+    private void getTimeTablePointCut() {}
+
     @Pointcut("execution(* dsm.service.schedule.service.ScheduleServiceImpl.updateScheduleService(..)) || " +
               "execution(* dsm.service.schedule.service.ScheduleServiceImpl.deleteScheduleService(..)) || " +
               "execution(* dsm.service.schedule.service.ScheduleServiceImpl.createScheduleService(..))")
@@ -26,6 +30,19 @@ public class ErrorHandling {
             return pjp.proceed();
         } catch (BusinessException businessException) {
             return GetScheduleResponse.newBuilder()
+                    .setStatus(businessException.getStatusCode())
+                    .setCode(businessException.getErrorCode())
+                    .setMsg(businessException.getMessage())
+                    .build();
+        }
+    }
+
+    @Around("getTimeTablePointCut()")
+    public Object getTimeTableErrorHandling(ProceedingJoinPoint pjp) throws Throwable {
+        try {
+            return pjp.proceed();
+        } catch (BusinessException businessException) {
+            return GetTimeTableResponse.newBuilder()
                     .setStatus(businessException.getStatusCode())
                     .setCode(businessException.getErrorCode())
                     .setMsg(businessException.getMessage())

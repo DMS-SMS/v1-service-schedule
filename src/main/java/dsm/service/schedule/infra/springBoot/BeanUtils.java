28ce8170e070a8f4a7fd24b319d8853ca261a65c
Maly <migsking@naver.com>
2020-10-12 03:01:28 +0900

    [CREATE] #22 SQL Tracing 을 위한 beanUtils

---
+++ src/main/java/dsm/service/schedule/infra/springBoot/BeanUtils.java
@@ -0,0 +1,10 @@
+package dsm.service.schedule.infra.springBoot;
+
+import org.springframework.context.ApplicationContext;
+
+public class BeanUtils {
+    public static Object getBean(String beanName) {
+        ApplicationContext applicationContext = ApplicationContextProvider.getApplicationContext();
+        return applicationContext.getBean(beanName);
+    }
+}

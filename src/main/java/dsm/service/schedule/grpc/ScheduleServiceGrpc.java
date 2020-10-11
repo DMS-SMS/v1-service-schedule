7d9d05c2cc8a0e258dced297b9082fe650d00a8e
Maly <migsking@naver.com>
2020-10-11 21:53:13 +0900

    [ADD] #22 Metadata 전달

--- src/main/java/dsm/service/schedule/grpc/ScheduleServiceGrpc.java
+++ src/main/java/dsm/service/schedule/grpc/ScheduleServiceGrpc.java
@@ -3,6 +3,8 @@ package dsm.service.schedule.grpc;
 import dsm.service.schedule.proto.*;
 import dsm.service.schedule.proto.ScheduleServiceGrpc.ScheduleServiceImplBase;
 import dsm.service.schedule.service.ScheduleService;
+import io.grpc.Context;
+import io.grpc.Metadata;
 import io.grpc.stub.StreamObserver;
 import lombok.AllArgsConstructor;
 import org.lognet.springboot.grpc.GRpcService;
@@ -12,33 +14,46 @@ import org.lognet.springboot.grpc.GRpcService;
 public class ScheduleServiceGrpc extends ScheduleServiceImplBase {
     private final ScheduleService scheduleService;
 
+    private final MetadataInterceptor metadataInterceptor;
+
     @Override
     public void createSchedule(CreateScheduleRequest request, StreamObserver<DefaultScheduleResponse> responseObserver) {
-        responseObserver.onNext(scheduleService.createScheduleService(request));
+        responseObserver.onNext(scheduleService.createScheduleService(request,
+                (String) MetadataInterceptor.xRequestId.get(),
+                (String) MetadataInterceptor.spanContext.get()));
         responseObserver.onCompleted();
     }
 
     @Override
     public void getSchedule(GetScheduleRequest request, StreamObserver<GetScheduleResponse> responseObserver) {
-        responseObserver.onNext(scheduleService.getScheduleService());
+        responseObserver.onNext(scheduleService.getScheduleService(request,
+                (String) MetadataInterceptor.xRequestId.get(),
+                (String) MetadataInterceptor.spanContext.get()));
         responseObserver.onCompleted();
     }
 
     @Override
     public void updateSchedule(UpdateScheduleRequest request, StreamObserver<DefaultScheduleResponse> responseObserver) {
-        responseObserver.onNext(scheduleService.updateScheduleService(request));
+        responseObserver.onNext(scheduleService.updateScheduleService(request,
+                (String) MetadataInterceptor.xRequestId.get(),
+                (String) MetadataInterceptor.spanContext.get()));
         responseObserver.onCompleted();
     }
 
     @Override
     public void deleteSchedule(DeleteScheduleRequest request, StreamObserver<DefaultScheduleResponse> responseObserver) {
-        responseObserver.onNext(scheduleService.deleteScheduleService(request));
+        responseObserver.onNext(scheduleService.deleteScheduleService(request,
+                (String) MetadataInterceptor.xRequestId.get(),
+                (String) MetadataInterceptor.spanContext.get()));
         responseObserver.onCompleted();
     }
 
     @Override
     public void getTimeTable(GetTimeTableRequest request, StreamObserver<GetTimeTableResponse> responseObserver) {
-        responseObserver.onNext(scheduleService.getTimeTableService(request));
+        responseObserver.onNext(scheduleService.getTimeTableService(
+                request,
+                (String) MetadataInterceptor.xRequestId.get(),
+                (String) MetadataInterceptor.spanContext.get()));
         responseObserver.onCompleted();
     }
 }

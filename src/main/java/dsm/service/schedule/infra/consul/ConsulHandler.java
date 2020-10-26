b8368dd38766ccd1bafd8db72144cc2de2e50cc0
Maly <migsking@naver.com>
2020-10-26 17:17:22 +0900

    [MODIFY] #33 Random port and uuid

--- src/main/java/dsm/service/schedule/infra/consul/ConsulHandler.java
+++ src/main/java/dsm/service/schedule/infra/consul/ConsulHandler.java
@@ -9,27 +9,31 @@ import com.orbitz.consul.model.agent.Registration;
 import com.orbitz.consul.model.health.Service;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.lognet.springboot.grpc.context.LocalRunningGrpcPort;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
 import java.util.Map;
 import java.util.Objects;
+import java.util.UUID;
 
 @Component
 public class ConsulHandler {
-    private String serviceId = "DMS.SMS.v1.service.schedule";
-    private Consul client = Consul.builder().withUrl("http://127.0.0.1:8500").build();
+    private String serviceName = "DMS.SMS.v1.service.schedule";
+    private String serviceId = String.format("%s-%s", serviceName, UUID.randomUUID());
+    private Consul client = Consul.builder().withUrl(String.format("http://%s", System.getenv("CONSUL_ADDR"))).build();
     private AgentClient agentClient = client.agentClient();
     private KeyValueClient keyValueClient = client.keyValueClient();
+    @LocalRunningGrpcPort private Integer port;
 
     public void registerConsul() throws NotRegisteredException {
         ImmutableRegistration service = ImmutableRegistration.builder()
                 .id(serviceId)
                 .name("DMS.SMS.v1.service.schedule")
-                .port(10444)
+                .port(port)
                 .check(Registration.RegCheck.ttl(1000000L))
                 .tags(Collections.singletonList("schedule"))
-                .meta(Collections.singletonMap("version","0.0.1"))
+                .meta(Collections.singletonMap("version","0.8"))
                 .build();
 
         agentClient.register(service);

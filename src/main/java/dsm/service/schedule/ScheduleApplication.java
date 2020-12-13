489c78a3ed4c3c23efc197b5513f02cafa3a7377
Maly <migsking@naver.com>
2020-12-14 04:29:43 +0900

    [ADD] Update Time Table

--- src/main/java/dsm/service/schedule/ScheduleApplication.java
+++ src/main/java/dsm/service/schedule/ScheduleApplication.java
@@ -2,8 +2,10 @@ package dsm.service.schedule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ScheduleApplication {
     public static void main(String[] args) {
         SpringApplication.run(ScheduleApplication.class, args);
---
+++ src/main/java/dsm/service/schedule/infra/schoolApi/SchoolApiServiceImpl.java
@@ -0,0 +1,54 @@
+package dsm.service.schedule.infra.schoolApi;
+
+import lombok.RequiredArgsConstructor;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.jsoup.Jsoup;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class SchoolApiServiceImpl implements SchoolApiService {
+    @Value("${schedule.schoolApi.key}")
+    private String key;
+
+    @Override
+    public Map<Integer, String> getTimeTable(Integer grade, Integer group, Integer year, Integer month, Integer day) {
+        try {
+            Map<Integer, String> tableMap = new java.util.HashMap<>(Map.of());
+            String json = Jsoup.connect(
+                    "https://open.neis.go.kr/hub/hisTimetable?" +
+                            "KEY="+key+
+                            "&Type=json" +
+                            "&ATPT_OFCDC_SC_CODE=G10" +
+                            "&SD_SCHUL_CODE=7430310" +
+                            "&GRADE="+grade.toString()+
+                            "&CLASS_NM="+group.toString()+
+                            "&ALL_TI_YMD="+year.toString()+month.toString()+day.toString())
+                    .ignoreContentType(true).execute().body();
+            JSONParser jsonParser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(json);
+            JSONArray hisTimetable = (JSONArray) jsonObject.get("hisTimetable");
+            JSONObject timetable = (JSONObject) hisTimetable.get(1);
+            JSONArray row = (JSONArray) timetable.get("row");
+
+            for (Object table: row) {
+                JSONObject jsonTable = (JSONObject) table;
+                tableMap.put(Integer.valueOf(jsonTable.get("PERIO").toString()),
+                        jsonTable.get("ITRT_CNTNT").toString());
+            }
+
+            return tableMap;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return Map.of();
+    }
+}

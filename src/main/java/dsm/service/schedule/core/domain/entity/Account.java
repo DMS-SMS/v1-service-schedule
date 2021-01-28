a59cdc96c63019d3997d4912ab2bb47fd7e738b6
Mallycrip <migsking@naver.com>
2021-01-29 07:46:10 +0900

    [CREATE] Account

---
+++ src/main/java/dsm/service/schedule/core/domain/entity/Account.java
@@ -0,0 +1,15 @@
+package dsm.service.schedule.core.domain.entity;
+
+import dsm.service.schedule.core.domain.entity.enums.AccountType;
+
+public class Account {
+    private Integer grade;
+
+    private Integer group;
+
+    private String name;
+
+    private String phoneNumber;
+
+    private AccountType type;
+}

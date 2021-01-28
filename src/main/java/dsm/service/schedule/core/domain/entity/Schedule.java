2f8023b44a5638047627f9d88e183c2243385e53
Mallycrip <migsking@naver.com>
2021-01-29 07:46:16 +0900

    [CREATE] Schedule

---
+++ src/main/java/dsm/service/schedule/core/domain/entity/Schedule.java
@@ -0,0 +1,13 @@
+package dsm.service.schedule.core.domain.entity;
+
+import java.time.LocalDate;
+
+public class Schedule {
+    private String scheduleUuid;
+
+    private String detail;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+}

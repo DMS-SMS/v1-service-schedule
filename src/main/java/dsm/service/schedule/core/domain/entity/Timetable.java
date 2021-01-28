f9d7b103aa51bc0ca0ffc70fc96a1991e944bc5a
Mallycrip <migsking@naver.com>
2021-01-29 07:46:29 +0900

    [CREATE] Timetable

---
+++ src/main/java/dsm/service/schedule/core/domain/entity/Timetable.java
@@ -0,0 +1,23 @@
+package dsm.service.schedule.core.domain.entity;
+
+public class Timetable {
+    private String timetableUuid;
+
+    private Integer targetGrade;
+
+    private Integer targetGroup;
+
+    private String firstSubject;
+
+    private String secondSubject;
+
+    private String thirdSubject;
+
+    private String fourthSubject;
+
+    private String fifthSubject;
+
+    private String sixthSubject;
+
+    private String seventhSubject;
+}

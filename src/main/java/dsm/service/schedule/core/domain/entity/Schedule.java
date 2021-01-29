b7fecaa9cf8191602ea5eb4066c5bbee4aa9098c
Mallycrip <migsking@naver.com>
2021-01-29 15:54:38 +0900

    [MODIFY] 다시 Getter Builder로 바꿈

--- src/main/java/dsm/service/schedule/core/domain/entity/Schedule.java
+++ src/main/java/dsm/service/schedule/core/domain/entity/Schedule.java
@@ -1,10 +1,12 @@
 package dsm.service.schedule.core.domain.entity;
 
-import lombok.Value;
+import lombok.Builder;
+import lombok.Getter;
 
 import java.time.LocalDate;
 
-@Value
+@Getter
+@Builder
 public class Schedule {
     String scheduleUuid;
 
@@ -13,4 +15,11 @@ public class Schedule {
     LocalDate startDate;
 
     LocalDate endDate;
+
+    public Schedule updateSchedule(String detail, LocalDate startDate, LocalDate endDate) {
+        this.detail = detail;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        return this;
+    }
 }

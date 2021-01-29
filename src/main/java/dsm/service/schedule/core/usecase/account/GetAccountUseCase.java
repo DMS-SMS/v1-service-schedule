fc2031e548851778aa18b8930935e76b99305a06
Mallycrip <migsking@naver.com>
2021-01-29 09:42:33 +0900

    [CREATE] GetAccount UseCase

---
+++ src/main/java/dsm/service/schedule/core/usecase/account/GetAccountUseCase.java
@@ -0,0 +1,39 @@
+package dsm.service.schedule.core.usecase.account;
+
+import dsm.service.schedule.core.domain.entity.Account;
+import dsm.service.schedule.core.domain.repository.AccountRepository;
+import dsm.service.schedule.core.usecase.UseCase;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class GetAccountUseCase extends UseCase<GetAccountUseCase.InputValues, GetAccountUseCase.OutputValues> {
+    private final AccountRepository accountRepository;
+
+    @Override
+    public OutputValues execute(InputValues input) {
+        return new OutputValues(getAccount(input.accountUuid));
+    }
+
+    private Account getAccount(String accountUuid) {
+        switch (accountUuid.substring(0, accountUuid.length() - 13)) {
+            case "admin": return accountRepository.findAdminByUuid(accountUuid, accountUuid);
+            case "student": return accountRepository.findStudentByUuid(accountUuid, accountUuid);
+            case "teacher": return accountRepository.findTeacherByUuid(accountUuid, accountUuid);
+            case "parents": return accountRepository.findParentsByUuid(accountUuid, accountUuid);
+            default: return null;
+        }
+    }
+
+    @Value
+    static public class InputValues implements UseCase.InputValues {
+        String accountUuid;
+    }
+
+    @Value
+    static public class OutputValues implements UseCase.OutputValues {
+        Account account;
+    }
+}

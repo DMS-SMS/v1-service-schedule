6a2ecb093372d13c5ce49c56005191a752b8ab4f
Mallycrip <migsking@naver.com>
2021-01-29 09:40:22 +0900

    [CREATE] Schedule Model

---
+++ src/main/java/dsm/service/schedule/data/db/jpa/model/ScheduleModel.java
@@ -0,0 +1,33 @@
+package dsm.service.schedule.data.db.jpa.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Builder
+@Getter
+@Entity(name="test_tbl_schedule")
+@NoArgsConstructor
+@AllArgsConstructor
+public class ScheduleModel {
+    @Id
+    private String uuid;
+
+    @Column(length = 20, nullable = false)
+    private String teacherUuid;
+
+    @Column(length = 100, nullable = false)
+    private String detail;
+
+    @Column(nullable = false)
+    private LocalDate startDate;
+
+    @Column(nullable = false)
+    private LocalDate endDate;
+}

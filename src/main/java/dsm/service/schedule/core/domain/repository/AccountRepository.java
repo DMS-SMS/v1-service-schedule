d3f15cdb91d3cbb38319e189006eca356aa4185c
Mallycrip <migsking@naver.com>
2021-01-29 10:04:58 +0900

    [MODIFY] Optional 추가

--- src/main/java/dsm/service/schedule/core/domain/repository/AccountRepository.java
+++ src/main/java/dsm/service/schedule/core/domain/repository/AccountRepository.java
@@ -3,13 +3,15 @@ package dsm.service.schedule.core.domain.repository;
 import dsm.service.schedule.core.domain.entity.Account;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AccountRepository {
-    public Account findAdminByUuid(String adminUuid, String accountUuid);
+    Optional<Account> findAdminByUuid(String adminUuid, String accountUuid);
 
-    public Account findStudentByUuid(String studentUuid, String accountUuid);
+    Optional<Account> findStudentByUuid(String studentUuid, String accountUuid);
 
-    public Account findTeacherByUuid(String teacherUuid, String accountUuid);
+    Optional<Account> findTeacherByUuid(String teacherUuid, String accountUuid);
 
-    public Account findParentsByUuid(String parentsUuid, String accountUuid);
+    Optional<Account> findParentsByUuid(String parentsUuid, String accountUuid);
 }

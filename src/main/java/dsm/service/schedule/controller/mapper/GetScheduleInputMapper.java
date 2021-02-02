6b393d212142f9b360c85b1f2ae99b9aeb11a0b5
Mallycrip <migsking@naver.com>
2021-02-03 03:25:59 +0900

    [FIX] Annotation 추가

--- src/main/java/dsm/service/schedule/controller/mapper/GetScheduleInputMapper.java
+++ src/main/java/dsm/service/schedule/controller/mapper/GetScheduleInputMapper.java
@@ -2,7 +2,9 @@ package dsm.service.schedule.controller.mapper;
 
 import dsm.service.schedule.core.usecase.schedule.GetScheduleUseCase;
 import dsm.service.schedule.proto.GetScheduleRequest;
+import org.springframework.stereotype.Component;
 
+@Component
 public class GetScheduleInputMapper {
     public GetScheduleUseCase.InputValues map(GetScheduleRequest request) {
         return new GetScheduleUseCase.InputValues(

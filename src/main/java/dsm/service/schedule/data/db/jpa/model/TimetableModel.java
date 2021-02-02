ba27af3a26715bb95c9f52cb00a8bcd935ce2262
Mallycrip <migsking@naver.com>
2021-02-02 11:58:52 +0900

    [CREATE] Timetable Model

---
+++ src/main/java/dsm/service/schedule/data/db/jpa/model/TimetableModel.java
@@ -0,0 +1,50 @@
+package dsm.service.schedule.data.db.jpa.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Getter
+@Builder
+@Table(indexes = {@Index(columnList = "targetGrade"), @Index(columnList = "targetGroup")})
+@Entity(name="test_tbl_timetable")
+@NoArgsConstructor
+@AllArgsConstructor
+public class TimetableModel {
+    @Id
+    private String uuid;
+
+    @Column(nullable = false)
+    private Integer targetGrade;
+
+    @Column(nullable = false)
+    private Integer targetGroup;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(length = 20)
+    private String firstSubject;
+
+    @Column(length = 20)
+    private String secondSubject;
+
+    @Column(length = 20)
+    private String thirdSubject;
+
+    @Column(length = 20)
+    private String fourthSubject;
+
+    @Column(length = 20)
+    private String fifthSubject;
+
+    @Column(length = 20)
+    private String sixthSubject;
+
+    @Column(length = 20)
+    private String seventhSubject;
+}

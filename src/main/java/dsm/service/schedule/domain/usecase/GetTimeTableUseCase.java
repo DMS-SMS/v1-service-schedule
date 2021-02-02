ef6402d3435dcbfacd3cc7fdadba7282f2d99d05
Mallycrip <migsking@naver.com>
2021-02-03 03:03:44 +0900

    [DELETE] *

--- src/main/java/dsm/service/schedule/domain/usecase/GetTimeTableUseCase.java
+++
@@ -1,7 +0,0 @@
-package dsm.service.schedule.domain.usecase;
-
-import dsm.service.schedule.domain.entity.Timetable;
-
-public interface GetTimeTableUseCase {
-    public Timetable execute(String uuid, Integer year, Integer month, Integer day);
-}

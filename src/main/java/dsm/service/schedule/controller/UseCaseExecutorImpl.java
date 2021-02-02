6a42ccdb88f4bf5bae67e0d75aa15d199103c59c
Mallycrip <migsking@naver.com>
2021-02-03 02:54:40 +0900

    [MODIFY] Executor 제네릭 수정

--- src/main/java/dsm/service/schedule/controller/UseCaseExecutorImpl.java
+++ src/main/java/dsm/service/schedule/controller/UseCaseExecutorImpl.java
@@ -3,11 +3,13 @@ package dsm.service.schedule.controller;
 import dsm.service.schedule.core.Mapper;
 import dsm.service.schedule.core.usecase.UseCase;
 import dsm.service.schedule.core.usecase.UseCaseExecutor;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UseCaseExecutorImpl implements UseCaseExecutor {
     @Override
     public <I extends UseCase.InputValues, O extends UseCase.OutputValues, Res> Res
-    execute(UseCase<I, O> useCase, I input, O output, Mapper<O, Res> outputMapper) {
+    execute(UseCase<I, O> useCase, I input, Mapper<O, Res> outputMapper) {
         return outputMapper.map(useCase.execute(input));
     }
 }

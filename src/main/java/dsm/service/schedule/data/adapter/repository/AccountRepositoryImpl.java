3c43e6a318069246495ea21ebc0c67366c2d9faf
Mallycrip <migsking@naver.com>
2021-02-03 02:55:06 +0900

    [MODIFY] 에러 핸들링 추가

--- src/main/java/dsm/service/schedule/data/adapter/repository/AccountRepositoryImpl.java
+++ src/main/java/dsm/service/schedule/data/adapter/repository/AccountRepositoryImpl.java
@@ -2,6 +2,7 @@ package dsm.service.schedule.data.adapter.repository;
 
 import dsm.service.schedule.core.domain.entity.Account;
 import dsm.service.schedule.core.domain.entity.enums.AccountType;
+import dsm.service.schedule.core.domain.exception.ServerErrorException;
 import dsm.service.schedule.core.domain.repository.AccountRepository;
 import dsm.service.schedule.data.adapter.repository.mapper.AccountRepositoryMapper;
 import dsm.service.schedule.data.grpc.auth.AuthService;
@@ -29,11 +30,27 @@ public class AccountRepositoryImpl implements AccountRepository {
 
     @Override
     public Optional<Account> findStudentByUuid(String studentUuid, String accountUuid) {
-        return accountRepositoryMapper.map(authService.getStudentInform(studentUuid, accountUuid));
+        try {
+            return accountRepositoryMapper.map(authService.getStudentInform(studentUuid, accountUuid));
+        } catch (InterruptedException e) {
+            throw new ServerErrorException("Auth server (Student) timeout (3s) on Schedule Service. Detail : \n "+ e.getMessage());
+        } catch (NullPointerException e) {
+            throw new ServerErrorException(503, "Can not found service in Consul");
+        } catch (Exception ignored) {
+            return Optional.empty();
+        }
     }
 
     @Override
     public Optional<Account> findTeacherByUuid(String teacherUuid, String accountUuid) {
-        return accountRepositoryMapper.map(authService.getTeacherInform(teacherUuid, accountUuid));
+        try {
+            return accountRepositoryMapper.map(authService.getTeacherInform(teacherUuid, accountUuid));
+        } catch (InterruptedException e) {
+            throw new ServerErrorException("Auth server (Student) timeout (3s) on Schedule Service. Detail : \n "+ e.getMessage());
+        } catch (NullPointerException e) {
+            throw new ServerErrorException(503, "Can not found service in Consul");
+        } catch (Exception ignored) {
+            return Optional.empty();
+        }
     }
 }

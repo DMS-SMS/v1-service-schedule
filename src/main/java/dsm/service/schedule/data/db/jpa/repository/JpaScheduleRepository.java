87c7ce9ab431474f3e242dbd3d22650742895ae9
Mallycrip <migsking@naver.com>
2021-02-03 04:18:44 +0900

    [DEBUG]

--- src/main/java/dsm/service/schedule/data/db/jpa/repository/JpaScheduleRepository.java
+++ src/main/java/dsm/service/schedule/data/db/jpa/repository/JpaScheduleRepository.java
@@ -1,17 +1,16 @@
 package dsm.service.schedule.data.db.jpa.repository;
 
 import dsm.service.schedule.data.db.jpa.model.ScheduleModel;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface JpaScheduleRepository extends JpaRepository<ScheduleModel, String> {
-    @Query("SELECT s " +
-            "FROM test_tbl_schedule s " +
+public interface JpaScheduleRepository extends CrudRepository<ScheduleModel, String> {
+    @Query("FROM test_tbl_schedule s " +
             "WHERE :date BETWEEN " +
             "FUNCTION('date_format', s.startDate, '%Y, %m') AND FUNCTION('date_format', s.endDate, '%Y, %m')")
     List<ScheduleModel> findByCorrectDate(@Param("date") String date);
